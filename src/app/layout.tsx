e7c7e4651f968ba48e85c7526b45dbea4ec804f6
Miguel Souza <miguel.souza@gxmed.com.br>
2024-11-06 09:56:26 -0300

    chore: add shadcn ui setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,34 @@
-import type { Metadata } from "next"
-import localFont from "next/font/local"
-import "./globals.css"
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
-})
+});
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
-})
+});
 
 export const metadata: Metadata = {
   title: "Finance AI",
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="pt-BR">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} dark antialiased`}
       >
         {children}
       </body>
     </html>
-  )
+  );
 }
