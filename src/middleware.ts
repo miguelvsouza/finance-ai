236ec939848e5a56aa7b5aef4f51d2e17bf265d8
Miguel Souza <miguel.souza@gxmed.com.br>
2024-11-13 11:17:31 -0300

    hotfix: fix middleware to ignore api routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -21,6 +21,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/((?!api|_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
   ],
 }
